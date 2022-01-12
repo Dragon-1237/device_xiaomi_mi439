44dc88547d08114a78b0835ad058ed10b262103b
hsx02 <hsx02self@gmail.com>
2022-01-12 16:58:58 +0530

    mi439: welcome spark-os!!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The SparkOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_mi439.mk
+    $(LOCAL_DIR)/spark_mi439.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_mi439-user \
-    lineage_mi439-userdebug \
-    lineage_mi439-eng
+    spark_mi439-user \
+    spark_mi439-userdebug \
+    spark_mi439-eng
